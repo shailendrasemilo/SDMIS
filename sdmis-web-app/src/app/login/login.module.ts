7edf6cb715249580e83bd2bb73eb5cabeeb40774
Prerit Mathur <pmathur.1968@gmail.com>
2020-10-27 11:15:03 +0530

    angular first commit

---
+++ sdmis-web-app/src/app/login/login.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { LoginCompComponent } from './login-comp/login-comp.component';
+
+
+
+@NgModule({
+  declarations: [LoginCompComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class LoginModule { }
