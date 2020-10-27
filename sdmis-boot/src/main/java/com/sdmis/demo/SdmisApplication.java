d0c9f554ec6548a27c93615c605294a031b7738b
Prerit Mathur <pmathur.1968@gmail.com>
2020-10-27 11:59:09 +0530

    boot commit

---
+++ sdmis-boot/src/main/java/com/sdmis/demo/SdmisApplication.java
@@ -0,0 +1,13 @@
+package com.sdmis.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SdmisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SdmisApplication.class, args);
+	}
+
+}
---
+++ sdmis-boot/src/test/java/com/sdmis/demo/SdmisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sdmis.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SdmisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
