8de9ab87471b3d6d7041e0d1a2e1849823ecb111
shailendra <shailendrasemilo@gmail.com>
2020-11-12 13:16:16 +0530

    script db

--- SDMIS/src/main/java/com/np/sdmis/controller/UserController.java
+++ SDMIS/src/main/java/com/np/sdmis/controller/UserController.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -45,6 +46,12 @@ public class UserController {
 
 	@PostMapping(value = "/saveStudentDetail")
 	public ResponseDTO saveStudentBasicInfo(@RequestBody RequestDTO requestDTO) {
+
+		return studentService.saveStudentBasicDetail(requestDTO);
+
+	}
+	@PutMapping(value = "/udateStudentDetail")
+	public ResponseDTO updateStudentBasicInfo(@RequestBody RequestDTO requestDTO) {
 
 		return studentService.saveStudentBasicDetail(requestDTO);
 
